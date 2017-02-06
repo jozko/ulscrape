ce32dab041ecc1b5d32b9a53086534525596e26c
Oto Brglez <otobrglez@gmail.com>
2017-02-06 21:52:02 +0100

    Improvements here and there,...

--- test/helpers/__init__.py
+++ test/helpers/__init__.py
@@ -6,8 +6,8 @@ def response_from(path, url=None, formdata=None):
     if url is None:
         url = "http://www.example.com"
 
-    res_dir = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.realpath(os.path.join(res_dir, "../requests", path))
+    res_d = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.realpath(os.path.join(res_d, "../requests", path))
 
     if not os.path.exists(file_path):
         raise Exception("Request was not found in file \"%s\"." % file_path)
@@ -19,12 +19,7 @@ def response_from(path, url=None, formdata=None):
     file_handler = open(file_path, 'r')
     file_content = file_handler.read()
 
-    response = HtmlResponse(
-        encoding='utf-8',
-        url=url,
-        request=request,
-        body=file_content
-    )
-
+    response = HtmlResponse(url=url, request=request,
+                            encoding='utf-8', body=file_content)
     file_handler.close()
     return response
--- test/spiders/test_uradni_list.py
+++ test/spiders/test_uradni_list.py
@@ -10,12 +10,18 @@ class TestUradniListSpider(unittest.TestCase):
     def setUp(self):
         self.spider = UradniListSpider()
 
+    def test_years(self):
+        spider = UradniListSpider(years="2000,2015")
+        self.assertEqual(len(spider.search_years()), 2)
+
     def test_parse_simple_index(self):
         fd = {'year': '2017'}
-        res = self.spider.parse(response_from('002-rezultati-2017.html', fd))
+        response = response_from('002-rezultati-2017.html', None, fd)
+        res = self.spider.parse(response)
         self.assertEqual(len(res), 1)
 
     def test_parse_with_more_index(self):
         fd = {'year': '2016'}
-        res = self.spider.parse(response_from('001-rezultati-2016.html', fd))
+        response = response_from('001-rezultati-2016.html', None, fd)
+        res = self.spider.parse(response)
         self.assertEqual(len(res), 11)
